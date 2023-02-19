6c9ddb5c8a2b8fbf141198c3dbf5b89a8f8c93b2
Jusdepixel <mandy@jusdepixel.fr>
2023-02-19 11:49:46 +0100

    ci: theme bootstrap

--- views/site/partials/_footer.php
+++ views/site/partials/_footer.php
@@ -1,4 +1,53 @@
-
 </div>
+<footer>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <a href="<?= URI ?>home" class="navbar-brand">
+                <i class="bi bi-c-circle me-2 ps-4"></i>CommerUp
+                <small><?= date("Y") ?></small>
+            </a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a
+                            class="nav-link <?= PAGE === 'contact' ? 'active' : '' ?>"
+                            href="/contact"
+                        >
+                            Nous contacter
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a
+                            class="nav-link <?= PAGE === 'about' ? 'active' : '' ?>"
+                            href="/about"
+                        >
+                            À propos
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a
+                            class="nav-link <?= PAGE === 'delivery' ? 'active' : '' ?>"
+                            href="/delivery"
+                        >
+                            Livraison
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a
+                            class="nav-link <?= PAGE === 'networks' ? 'active' : '' ?>"
+                            href="/networks"
+                        >
+                            Réseaux sociaux
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</footer>
+
+<script src="<?= JS ?>/bootstrap.bundle.min.js"></script>
+<script src="<?= JS ?>/scripts.js"></script>
 </body>
 </html>
--- views/site/partials/_nav.php
+++ views/site/partials/_nav.php
@@ -1,19 +1,86 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="<?= $uri ?>pages/home">Commerup</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand ps-4" href="/">
+            <img src="<?= IMG . "/logo_light.png" ?>" height="50" alt="Commerup">
+            CommerUp
+        </a>
+        <button class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+        >
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a
+                        class="nav-link <?= PAGE === null ? 'active' : '' ?>"
+                        href="/"
+                    >
+                        <i class="bi bi-house me-1"></i>
+                        Accueil
+                    </a>
+                </li>
                 <?php if (!isAuthenticated()) { ?>
-                    <li class="nav-item"><a class="nav-link" href="<?= $uri ?>user/authenticate">Authenticate</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= $uri ?>user/cart">Cart</a></li>
+                    <li class="nav-item">
+                        <a 
+                            class="nav-link <?= PAGE === 'user/authenticate' ? 'active' : '' ?>"
+                            href="/user/authenticate"
+                        >
+                            <i class="bi bi-person me-1"></i>
+                            Mon compte
+                        </a>
+                    </li>
                 <?php } else { ?>
-                    <li class="nav-item"><a class="nav-link" href="<?= $uri ?>user/logout">Logout</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= $uri ?>user/orders">Orders</a></li>
-                    <li class="nav-item"><a class="nav-link" href="<?= $uri ?>user/address">Address</a></li>
+                    <li class="nav-item dropdown">
+                        <a
+                           href="#"
+                           role="button"
+                           data-bs-toggle="dropdown"
+                           aria-expanded="false" class="nav-link dropdown-toggle <?= (
+                            PAGE === 'user/orders' ||
+                            PAGE === 'user/logout' ||
+                            PAGE === 'user/account'
+                        ) ? 'active' : ''?>">
+                            <i class="bi bi-person me-1"></i>
+                            <?= getSession()['user']['firstname'] ?>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a class="dropdown-item <?= PAGE === 'user/account' ? 'active' : '' ?>"
+                                   href="/user/account">
+                                    Mon compte
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item <?= PAGE === 'user/orders' ? 'active' : '' ?>"
+                                    href="/user/orders">
+                                    Mes commandes
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item <?= PAGE === 'user/authenticate' ? 'active' : '' ?>"
+                                   href="/user/logout">
+                                    Se déconnecter
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
                 <?php } ?>
+                <li class="nav-item">
+                    <a
+                        class="nav-link  <?= PAGE === 'cart' ? 'active' : '' ?>"
+                        href="/cart"
+                    >
+                        <i class="bi bi-cart me-1"></i>
+                        Panier
+                    </a>
+                </li>
             </ul>
         </div>
     </div>
